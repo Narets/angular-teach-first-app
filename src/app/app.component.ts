ff0e1d4709bdf588ee4e187317e48a45807be525
Narets <Naret.ng@ku.th>
2019-02-10 14:33:56 +0700

    Narets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { Card } from './card';
 
-@Component({
+@Component({  
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
+  cards: Card[] =[]; //Creat array for save fistname lastname
+
+  get sorted(): Card[]{
+    console.log("assess");
+    return this.cards.sort( 
+      (a,b)=>b.votes - a.votes 
+      ); //sort( (a,b)=> ); ฟั่งชั้นในฟังชั่น
+
+  }
+
+  onclick(firstname:HTMLInputElement,lastname:HTMLInputElement){
+    console.log(firstname.value+"  "+lastname.value);
+    const card = new Card(firstname.value,lastname.value,0);
+    this.cards.push(card); //save string last row of array
+    console.log(this.cards);
+
+    firstname.value="";
+    lastname.value="";
+  }
+
+
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Card } from '../card';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  @Input() card:Card; //card.ts
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  addvotes(){
+    this.card.votes++;
+  }
+  Negvotes(){
+    this.card.votes--;
+  }
+
+}
